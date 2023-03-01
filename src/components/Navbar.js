cb5fca2195108fd17c77b87c957f75c6f5b96bc2
Johnny Sanchez <johnnyss1999@gmail.com>
2023-03-01 16:59:32 -0500

    Styled anchors (links)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
   return (
     <div className='navbar'>
 
-        <div className='header_logo'>
+        <div className='navbar_logo'>
             <span className='hack_for'>hack for </span>
             <span className='carrot'>{'<'}</span>
             <span className='good'>good</span>
@@ -14,18 +14,18 @@ function Navbar() {
             <span className='_2023'>2023</span>
         </div>
 
-        <div className='header_links'>
+        <div className='navbar_links'>
 
-            <div className='header_link'>
+            <div className='navbar_link'>
                 <h3>Link 1</h3>
             </div>
 
-            <div className='header_link'>
+            <div className='navbar_link'>
                 <h3>Link 2</h3>
             </div>
 
             <Link to='/login'>
-                <div className='header_link'>
+                <div className='navbar_link'>
                     <h3>Sign In</h3>
                 </div>
             </Link>
